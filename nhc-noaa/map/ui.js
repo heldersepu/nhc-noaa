3899d8efd868a9dbc5429d2face7eb28a2fa3193
Helder Sepulveda <heldersepu@gmail.com>
2018-05-30 08:13:51 -0400

    Add some keyShortcuts

--- nhc-noaa/map/ui.js
+++ nhc-noaa/map/ui.js
@@ -5,9 +5,19 @@ function resizeCanvas() {
 }
 
 function keyShortcuts(e) {
-    console.log(e.charCode)
-    if(e.charCode == 103) {
-      // Your Code
+    // Hide the action pane
+    if (e.charCode == 104 || e.charCode == 72) {
+        $("#action").hide("slow");
+    }
+    // Show the action pane
+    if (e.charCode == 115 || e.charCode == 83) {
+        $("#action").show("slow");
+    }
+    // Change speed of the map with the + and -
+    if (e.charCode == 43 || e.charCode == 45) {
+        var x = (e.charCode == 43) ? 2 : -2;
+        $("#speed").val(parseInt($("#speed").val()) + x)
+        $("#speed").change()
     }
 }
 
