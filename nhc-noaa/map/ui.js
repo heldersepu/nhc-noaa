8770bba618d42b68e049d61382b636c5b676cb53
Helder Sepulveda <heldersepu@gmail.com>
2018-05-30 07:54:15 -0400

    Testing keyShortcuts

--- nhc-noaa/map/ui.js
+++ nhc-noaa/map/ui.js
@@ -4,13 +4,21 @@ function resizeCanvas() {
     htmlCanvas.height = window.innerHeight;
 }
 
+function keyShortcuts(e) {
+    console.log(e.charCode)
+    if(e.charCode == 103) {
+      // Your Code
+    }
+}
+
 $(window).load(function () {
     resizeCanvas();
     window.addEventListener('resize', resizeCanvas, false);
+    $(document).keypress(keyShortcuts);
 
     var hash = window.location.hash.replace("#", "");
     if ($.isNumeric(hash)) $("#count").val(hash);
-    
+
     $("#back").click(function () { Map.changePos(-1); });
     $("#forw").click(function () { Map.changePos(1); });
     $("#speed").change(Map.changeSpeed);
