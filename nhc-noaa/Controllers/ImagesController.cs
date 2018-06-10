91a6d05a091b38a242542ee446ba49045b2af3e6
Helder <heldersepu@gmail.com>
2018-06-10 19:24:07 -0400

    testing a way to retrieve images from the storage

--- nhc-noaa/Controllers/ImagesController.cs
+++ nhc-noaa/Controllers/ImagesController.cs
@@ -2,6 +2,8 @@
 using System.Web.Http;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.File;
 
 namespace nhc_noaa.Controllers
 {
@@ -11,6 +13,17 @@ namespace nhc_noaa.Controllers
         public IEnumerable<string> EastAtlantic(int count = 20, DateTime? min = null, DateTime? max = null)
         {
             return DirInfo.GetLatestFiles(count, min, max).OrderByDescending(x => x.Name).Select(x => x.Name);
+        }
+
+        [HttpPost]
+        public IEnumerable<string> ImageData(int count = 20, DateTime? min = null, DateTime? max = null)
+        {
+            var storageAccount = CloudStorageAccount.Parse(ConnectionString);
+            var fileClient = storageAccount.CreateCloudFileClient();
+            var share = fileClient.GetShareReference("images");
+            var dir = share.GetRootDirectoryReference();
+            var files = dir.ListFilesAndDirectories().OrderByDescending(x => ((CloudFile)x).Name).Take(count);
+            return files.Select(x => ((CloudFile)x).Name);
         }
     }
 }
