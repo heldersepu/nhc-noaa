24e1eea719d5aa8f2b064691178240af2245e904
Helder Sepulveda <heldersepu@gmail.com>
2018-06-10 14:28:12 -0400

    Add logging to the MigrateController

--- nhc-noaa/Controllers/MigrateController.cs
+++ nhc-noaa/Controllers/MigrateController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using NLog;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -6,13 +8,23 @@ namespace nhc_noaa.Controllers
 {
     public class MigrateController : BaseController
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         public async Task<IEnumerable<string>> Get(int count = 5)
         {
-            var files = DirInfo.GetOldestFiles(count);
-            var fileNames = files.Select(x => x.Name).ToArray();
-            foreach (var file in files)
-                await file.Upload(ConnectionString);
-            return fileNames;
+            try
+            {
+                var files = DirInfo.GetOldestFiles(count);
+                var fileNames = files.Select(x => x.Name).ToArray();
+                foreach (var file in files)
+                    await file.Upload(ConnectionString);
+                return fileNames;
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+                throw e;
+            }
         }
     }
 }
