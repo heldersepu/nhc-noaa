84970804bff2558029929614f335bac80dbc2437
Helder Sepulveda <heldersepu@gmail.com>
2018-06-10 14:07:31 -0400

    Add optional param

--- nhc-noaa/Controllers/MigrateController.cs
+++ nhc-noaa/Controllers/MigrateController.cs
@@ -6,14 +6,12 @@ namespace nhc_noaa.Controllers
 {
     public class MigrateController : BaseController
     {
-        public async Task<IEnumerable<string>> Get()
+        public async Task<IEnumerable<string>> Get(int count = 5)
         {
-            var files = DirInfo.GetOldestFiles(5);
+            var files = DirInfo.GetOldestFiles(count);
             var fileNames = files.Select(x => x.Name).ToArray();
             foreach (var file in files)
-            {
                 await file.Upload(ConnectionString);
-            }
             return fileNames;
         }
     }
