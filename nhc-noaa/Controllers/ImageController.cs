1505233450ee0223dfccf503ab2d9a8b3a7d1856
Helder Sepulveda <heldersepu@gmail.com>
2018-06-17 08:32:35 -0400

    Complete action to get image from storage

--- nhc-noaa/Controllers/ImageController.cs
+++ nhc-noaa/Controllers/ImageController.cs
@@ -1,10 +1,23 @@
-﻿namespace nhc_noaa.Controllers
+﻿using Swagger.Net.Annotations;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace nhc_noaa.Controllers
 {
     public class ImageController : BaseController
     {
-        public string Get(string name)
+        [SwaggerResponse(200, mediaType: "image/png")]
+        public async Task<HttpResponseMessage> Get(string name)
         {
-            return CloudDir.GetFileReference(name).Uri.AbsolutePath;
-        }        
+            var response = new HttpResponseMessage();
+            var memStream = new MemoryStream();
+            await CloudDir.GetFileReference(name).DownloadRangeToStreamAsync(memStream, null, null);
+            memStream.Position = 0;
+            response.Content = new StreamContent(memStream);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+            return response;
+        }
     }
 }
