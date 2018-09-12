31049945068910b7ebcd0586641d270e58f48434
Helder Sepulveda <heldersepu@gmail.com>
2018-09-11 22:08:14 -0400

    Add CacheOutput

--- nhc-noaa/Controllers/ImagesController.cs
+++ nhc-noaa/Controllers/ImagesController.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using WebApi.OutputCache.V2;
 
 namespace nhc_noaa.Controllers
 {
     public class ImagesController : BaseController
     {
         [HttpGet]
+        [CacheOutput(ClientTimeSpan = 60, ServerTimeSpan = 60)]
         public IEnumerable<string> EastAtlantic(int count = 20, DateTime? min = null, DateTime? max = null)
         {
             var files = CloudDir.ListFilesAndDirectories().OrderByDescending(x => ((CloudFile)x).Name).Take(count);
